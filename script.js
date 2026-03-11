04a740e818d07f553ae1b842ff26337951004d86
isabellarfs <isabellarfs2024@gmail.com>
2026-03-11 08:07:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function subscribe() {
+alert("Thank you for subscribing.");
+}
+
+function addToCart() {
+alert("Item added to the cart.");
+}
+
+function clearCart() {
+alert("Cart cleared.");
+}
+
+function processOrder() {
+alert("Thank you for your order.");
+}
+
+function submitForm() {
+alert("Thank you for your message.");
+}
