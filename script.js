4dc49eac4b0e8fec909f427a9bf8e140fece9ce9
Isabella Ribeiro <isabellarfs2024@gmail.com>
2026-03-12 10:15:08 -0400

    fix javascript cart functions

--- script.js
+++ script.js
@@ -5,16 +5,9 @@ alert("Thank you for subscribing.");
 // CART STORAGE
 let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
-// ADD TO CART
-function addToCart(item) {
-cart.push(item);
-sessionStorage.setItem("cart", JSON.stringify(cart));
-alert("Item added to cart.");
-}
-
 // VIEW CART
 function viewCart() {
-let cartItems = JSON.parse(sessionStorage.getItem("cart")) || {};
+let cartItems = JSON.parse(sessionStorage.getItem("cart")) || [];
 let display = document.getElementById("cartDisplay");
 
 if(cartItems.length === 0){
