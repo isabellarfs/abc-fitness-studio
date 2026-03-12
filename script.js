57eaf9271566d317bc92e43b7c672aa474766d15
Isabella Ribeiro <isabellarfs2024@gmail.com>
2026-03-12 13:22:53 -0400

    fix javascript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function subscribe() {
 alert("Thank you for subscribing.");
 }
@@ -6,9 +5,18 @@ alert("Thank you for subscribing.");
 // CART STORAGE
 let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
+// ADD TO CART
+function addToCart(item) {
+cart.push(item);
+sessionStorage.setItem("cart", JSON.stringify(cart));
+alert("Item added to the cart.");
+}
+
 // VIEW CART
 function viewCart() {
+
 let cartItems = JSON.parse(sessionStorage.getItem("cart")) || [];
+
 let display = document.getElementById("cartDisplay");
 
 if(cartItems.length === 0){
@@ -17,6 +25,7 @@ display.innerHTML = "Your cart is empty.";
 else{
 display.innerHTML = "Items in cart: " + cartItems.join(", ");
 }
+
 }
 
 // CLEAR CART
@@ -29,31 +38,4 @@ alert("Cart cleared.");
 function processOrder() {
 alert("Thank you for your order!");
 sessionStorage.removeItem("cart");
-}
-
-// CUSTOMER FEEDBACK
-function saveFeedback() {
-let message = document.getElementById("message").value;
-localStorage.setItem("customerFeedback", message);
-alert("Thank you for your message.");
-=======
-function subscribe() {
-alert("Thank you for subscribing.");
-}
-
-function addToCart() {
-alert("Item added to the cart.");
-}
-
-function clearCart() {
-alert("Cart cleared.");
-}
-
-function processOrder() {
-alert("Thank you for your order.");
-}
-
-function submitForm() {
-alert("Thank you for your message.");
->>>>>>> 04a740e818d07f553ae1b842ff26337951004d86
 }
